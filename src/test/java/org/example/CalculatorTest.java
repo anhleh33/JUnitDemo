f68f9b91c83d0b822a74aae5e0a4ad80d2e94e48
Anh Le Hoang <anhlehoang2004@gmail.com>
2025-09-16 10:18:52 +0700

    first init

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calc.add( 2,  3));
+    }
+
+    @Test
+    void testSub() {
+        assertEquals(5, calc.sub(8, 3));
+    }
+
+    @Test
+    void testMul() {
+        assertEquals(5, calc.mul(2,  3));
+    }
+
+    @Test
+    void testDivWithZero() {
+        assertThrows(ArithmeticException.class, () -> {calc.div(2,  0);});
+    }
+
+    @Test
+    void testDiv() {
+        assertEquals(5, calc.div(2,  3));
+    }
+}
